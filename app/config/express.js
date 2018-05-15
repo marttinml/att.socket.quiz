7babcbfa2686cfda1b542eba0309705c955ea67a
Martín Mtz <martin.ml@icloud.com>
2018-05-15 14:32:25 -0500

    *

--- app/config/express.js
+++ app/config/express.js
@@ -6,6 +6,18 @@ var express         = require("express"),
 
 module.exports = function(){
 
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+
+    app.use(function (req, res, next) {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        res.setHeader('Access-Control-Allow-Credentials', true);
+        next();
+    });
+    
     var VERSIONS = {'Pre-Production': '/v0/'};
     app.get('/', function(req, res) {
         res.json(VERSIONS);
