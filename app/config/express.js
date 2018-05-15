0a0dfa32a397da451be978e6c91991b60320cd40
Martín Mtz <martin.ml@icloud.com>
2018-05-15 14:42:41 -0500

    url

--- app/config/express.js
+++ app/config/express.js
@@ -11,13 +11,13 @@ module.exports = function(){
     }));
 
     app.use(function (req, res, next) {
-        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Origin', 'https://secure-mesa-50981.herokuapp.com/');
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
         res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
         res.setHeader('Access-Control-Allow-Credentials', true);
         next();
     });
-    
+
     var VERSIONS = {'Pre-Production': '/v0/'};
     app.get('/', function(req, res) {
         res.json(VERSIONS);
